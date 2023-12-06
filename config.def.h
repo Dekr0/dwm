f8fa3a6a7feb22bdbbfd2e44e52541761a2d5c85
Dekr0 <dekr0.dk@protonmail.com>
2023-12-06 15:40:14 -0700

    sync config

--- config.def.h
+++ config.def.h
@@ -1,25 +1,35 @@
-/* See LICENSE file for copyright and license details. */
-
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const int showbar            = 0;        /* 0 means no bar */
+static const int topbar             = 0;        /* 0 means bottom bar */
+
+/* fonts */
+
+/* defaults */
+// static const char *fonts[]          = { "monospace:size=10" };
+
+static const char *fonts[]             = { "CaskaydiaCove Nerd Font:size=10:antialias=true:autohint=true" };
+
+/* theme */
+#include "theme.h"
+
+/* default */
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
 
+/* tokyo night */
+// static const char *colors[][3] = {
+//     /*                     fg       bg      border */
+//     [SchemeNorm]       = { white,   black,  gray2 },
+//     [SchemeSel]        = { black,   blue,   blue  },
+// };
+
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "", "", "", "󰘐", "5", "", "7", "8", "󰙯  " };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -27,8 +37,14 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+    { "Alacritty", NULL,       NULL,       1 << 0,       0,           -1 },
+	{ "Chromium",  NULL,       NULL,       1 << 1,       0,           -1 },
+    { "obsidian" , NULL,       NULL,       1 << 2,       0,           -1 },
+    { "dbeaver",   NULL,       NULL,       1 << 3,       0,           -1 },
+    { "code",      NULL,       NULL,       1 << 3,       0,           -1 },
+	{ "Gimp",      NULL,       NULL,       1 << 5,       0,           -1 },
+    { "spotify",   NULL,       NULL,       1 << 8,       0,           -1 },
+    { "Discord",   NULL,       NULL,       1 << 8,       0,           -1 }
 };
 
 /* layout(s) */
@@ -45,7 +61,11 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
+#include <X11/XF86keysym.h> // For function keys
+#include "command.h"
+
 #define MODKEY Mod1Mask
+#define WINKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -55,15 +75,10 @@ static const Layout layouts[] = {
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
-/* commands */
-static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
-
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+
+    /* windows management */
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -73,7 +88,6 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
@@ -85,6 +99,8 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+
+    /* tag */
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
@@ -94,7 +110,28 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+    { MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
+	{ MODKEY|ShiftMask,             XK_0,      quit,           {0} },
+	{ MODKEY|ShiftMask,             XK_BackSpace, spawn,             {.v = shutdown} },
+	{ MODKEY|ControlMask,           XK_BackSpace, spawn,             {.v = reboot} },
+
+    /* application */
+    { WINKEY,                       XK_b,      spawn,          {.v = browser } },
+    { WINKEY,                       XK_c,      spawn,          {.v = discord } },
+    { WINKEY,                       XK_g,      spawn,          {.v = gimp }},
+    { WINKEY,                       XK_o,      spawn,          {.v = obsidian }},
+    { MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+    { ControlMask|ShiftMask,        XK_s,      spawn,          {.v = flameshot } },
+    { WINKEY|ShiftMask,             XK_d,      spawn,          {.v = rofi_run } },
+    { WINKEY,                       XK_d,      spawn,          {.v = rofi_drun } },
+    { WINKEY,                       XK_w,      spawn,          {.v = rofi_window } },
+    { WINKEY,                       XK_Return, spawn,          {.v = terminal } },
+    { WINKEY|MODKEY,                XK_f,      spawn,          {.v = wmname } },
+    { 0,                            XF86XK_MonBrightnessUp,    spawn, {.v = incr_brightness } },
+    { 0,                            XF86XK_MonBrightnessDown,  spawn, {.v = decr_brightness } },
+    { 0,                            XF86XK_AudioLowerVolume, spawn, {.v = downvol } },
+    { 0,                            XF86XK_AudioMute,        spawn, {.v = mutevol } },
+    { 0,                            XF86XK_AudioRaiseVolume, spawn, {.v = upvol   } },
 };
 
 /* button definitions */
@@ -104,7 +141,7 @@ static const Button buttons[] = {
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
-	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
+	{ ClkStatusText,        0,              Button2,        spawn,          {.v = terminal } },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
