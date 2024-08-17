ef81dbcdb62d7bca6a15a92d270e5fa7f2d0a87f
Dekr0 <dekr0.dk@protonmail.com>
2024-08-16 18:53:12 -0600

    remove unused command and update commands

--- command.h
+++ command.h
@@ -7,23 +7,20 @@ static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", "Caskaydia
 static const char *obsidian[] = { "obsidian", NULL };
 static const char *gimp[] = { "gimp", NULL };
 static const char *flameshot[] = { "flameshot", "gui", NULL };
-static const char *terminal[]  = { "wezterm", NULL };
-// static const char *terminal[]  = { "st", "-e", "/usr/bin/tmux", NULL };
-static const char *wmname[] = { "wmname", "LG3D", NULL };
+static const char *terminal[]  = { "alacritty", NULL };
 
 static const char *rofi_run[] = { "rofi", "-show", "run", NULL };
 static const char *rofi_drun[] = { "rofi", "-show", "drun", NULL };
 static const char *rofi_window[] = { "rofi", "-show", "window", NULL };
-
-/* power */
-static const char *reboot[] = { "reboot", NULL };
-static const char *shutdown[] = { "shutdown", "now", NULL };
 
 /* laptop brightness */
 static const char *incr_brightness[] = { "xbacklight", "-inc", "5", NULL };
 static const char *decr_brightness[] = { "xbacklight", "-dec", "5", NULL };
 
 /* volume */
-static const char *upvol[]   = { "/usr/bin/pactl", "set-sink-volume", "@DEFAULT_SINK@", "+5%", NULL };
-static const char *downvol[] = { "/usr/bin/pactl", "set-sink-volume", "@DEFAULT_SINK@", "-5%", NULL };
-static const char *mutevol[] = { "/usr/bin/pactl", "set-sink-mute",   "@DEFAULT_SINK@", "toggle", NULL };
+static const char *upvol[]   = { "/usr/bin/pactl", 
+    "set-sink-volume", "@DEFAULT_SINK@", "+5%", NULL };
+static const char *downvol[] = { "/usr/bin/pactl", 
+    "set-sink-volume", "@DEFAULT_SINK@", "-5%", NULL };
+static const char *mutevol[] = { "/usr/bin/pactl", 
+    "set-sink-mute",   "@DEFAULT_SINK@", "toggle", NULL };
