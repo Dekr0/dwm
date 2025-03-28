8d3dc48dc7a1c7fc21c42dac02a598f13d2e3456
Dekr0 <dekr0.dk@protonmail.com>
2025-03-28 03:22:12 -0600

    update

--- config.def.h
+++ config.def.h
@@ -9,7 +9,7 @@ static char dmenumon[2] = "0";
 static const char *dmenucmd[] = {
     "dmenu_run",
     "-m", dmenumon,
-    "-fn", "Jetbrain Mono:size=10",
+    "-fn", "Iosevka:size=10",
     NULL
 };
 
@@ -59,7 +59,7 @@ static const int showbar            = 0;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 
 /* fonts */
-static const char *fonts[] = { "Jetbrain Mono:size=10:antialias=true:autohint=true" };
+static const char *fonts[] = { "Iosevka:size=10:antialias=true:autohint=true" };
 
 /* theme */
 static const char col_gray1[]       = "#222222";
